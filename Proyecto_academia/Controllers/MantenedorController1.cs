7848bfc895d97d6bc5e0008b5c405ac7cc27c817
mario <mario@LAPTOP-E44GU85B>
2023-05-09 16:07:21 -0600

    holaactualizacionholaAgregar archivos de proyecto.

---
+++ Proyecto_academia/Controllers/MantenedorController1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto_academia.Datos;
+using Proyecto_academia.Models;
+
+namespace Proyecto_academia.Controllers
+{
+    public class MantenedorController1 : Controller
+    {
+        DatosUsuarios _UsuariosDatos = new DatosUsuarios();
+        public IActionResult Listar()
+        {
+            //La vista mostrara una lista 
+            var oLista = _UsuariosDatos.Listar();
+
+            return View(oLista);
+        }
+        public IActionResult Guardar()
+        {
+            //metodo solo devuelve la vista
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Guardar(Usuarios ousuarios)
+        {
+            //recibe el objeto para guardarlo bd
+            var respuesta = _UsuariosDatos.Guardar(ousuarios);
+            if (respuesta)
+                 return RedirectToAction("Listar");
+            else
+                return View();
+        }
+
+    }
+}
