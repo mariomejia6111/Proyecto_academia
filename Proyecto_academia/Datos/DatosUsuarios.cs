d6e4287529528db67782b8b8a628b9b550b3eee3
mario <mario@LAPTOP-E44GU85B>
2023-05-09 16:07:21 -0600

    holaactualizacionholaAgregar archivos de proyecto.

--- Proyecto_academia/Datos/DatosUsuarios.cs
+++ Proyecto_academia/Datos/DatosUsuarios.cs
@@ -9,16 +9,20 @@ namespace Proyecto_academia.Datos
 
         public List<Usuarios> Listar()
         {
-           var oLista = new List<Usuarios>();
+
+            var oLista = new List<Usuarios>();
+
             var cn = new Conexion();
 
             using (var conexion = new SqlConnection(cn.getCadenaSQL()))
             {
                 conexion.Open();
-                SqlCommand cmd = new SqlCommand("select * from Usuarios;",conexion);
+                SqlCommand cmd = new SqlCommand("sp_Listar", conexion);
                 cmd.CommandType = CommandType.StoredProcedure;
+
                 using (var dr = cmd.ExecuteReader())
                 {
+
                     while (dr.Read())
                     {
                         oLista.Add(new Usuarios()
@@ -27,14 +31,16 @@ namespace Proyecto_academia.Datos
                             Correo = dr["Correo"].ToString(),
                             Nombre_User = dr["NombreUsuario"].ToString(),
                             Contra = dr["Contra"].ToString(),
-                            FechaCreacion = Convert.ToDateTime(dr["FechaCreacion"]),
+                            FechaCreacion = Convert.ToDateTime(dr["FechaCreacion"])
+                        });
 
-                        }) ;
                     }
                 }
             }
+
             return oLista;
         }
+
 
         public bool Guardar(Usuarios ousuarios)
         {
@@ -42,24 +48,96 @@ namespace Proyecto_academia.Datos
 
             try
             {
-
                 var cn = new Conexion();
-                using (var conexion = new SqlConnection())
+
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
                 {
                     conexion.Open();
-                    SqlCommand cmd = new SqlCommand("", conexion);
+                    SqlCommand cmd = new SqlCommand("sp_Guardar", conexion);
+                    cmd.Parameters.AddWithValue("Correo", ousuarios.Correo);
+                    cmd.Parameters.AddWithValue("NombreUsuario", ousuarios.Nombre_User);
+                    cmd.Parameters.AddWithValue("Contra", ousuarios.Contra);
+                    cmd.Parameters.AddWithValue("FechaCreacion", ousuarios.FechaCreacion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
                 }
+                rpta = true;
 
-
-            }catch (Exception e)
-            {
 
             }
+            catch (Exception e)
+            {
+
+                string error = e.Message;
+                rpta = false;
+            }
+
+
 
             return rpta;
         }
-       
-       
+
+        public bool Editar(Usuarios ousuarios)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Editar", conexion);
+                    cmd.Parameters.AddWithValue("IdUsuario", ousuarios.IdUsuario);
+                    cmd.Parameters.AddWithValue("Correo", ousuarios.Correo);
+                    cmd.Parameters.AddWithValue("NombreUsuario", ousuarios.Nombre_User);
+                    cmd.Parameters.AddWithValue("Contra", ousuarios.Contra);
+                    cmd.Parameters.AddWithValue("FechaCreacion", ousuarios.FechaCreacion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+
+
+            }
+            catch (Exception e)
+            {
+
+                string error = e.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+
+        public bool Eliminar(int IdUsuario)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Eliminar", conexion);
+                    cmd.Parameters.AddWithValue("IdUsuario", IdUsuario);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+
+
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+
     }
    
 }
