e8c87847ff8ac9bfcd67e868f847b86017bb3a70
mario <mario@LAPTOP-E44GU85B>
2023-05-09 16:07:21 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_academia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_academia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
