663218934b85bc010140fec47d33be57cbb90434
mario <mario@LAPTOP-E44GU85B>
2023-05-10 17:23:29 -0600

    05:23

--- Proyecto_academia/Program.cs
+++ Proyecto_academia/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=MantenedorController1}/{action=Listar}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
