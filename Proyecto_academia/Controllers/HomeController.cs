b9748743f7f7cfc869cdfbec1129732bec373e7f
vicki197 <43527053+vicki197@users.noreply.github.com>
2023-05-26 16:40:11 -0600

    nosotros

--- Proyecto_academia/Controllers/HomeController.cs
+++ Proyecto_academia/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Proyecto_academia.Controllers
             return View();
         }
 
+        public IActionResult Nosotros()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
