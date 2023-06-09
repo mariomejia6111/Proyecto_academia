be82d5bc64628c60d193ddc9b3fc7165a83fea97
pc <pc@DESKTOP-4RJC9L1>
2023-06-09 15:58:08 -0600

    hola

--- Proyecto_academia/Program.cs
+++ Proyecto_academia/Program.cs
@@ -31,7 +31,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Curso}/{action=Cursos}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
